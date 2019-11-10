2445d47d4f70d99992b1da094e5502849330aa73
VictorMoraJimenez <vmoraji@gmail.com>
2019-11-10 22:53:50 +0100

    Improve descriptions code

--- EchoServer.py
+++ EchoServer.py
@@ -2,34 +2,37 @@
 import socket
 import sys
 
+# IP, PORT AND BUFFERSIZE CONNECTION
+SRV_ADDR = ('192.168.10.1')
+PORT = 7
+BUFF_SIZE = 64
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('192.168.10.1', 7)
-print('Open socket on {} port {}'.format(*server_address))
-sock.bind(server_address)
+print('Open socket on {} port {}'.format(SRV_ADDR,PORT))
+sock.bind((SRV_ADDR,PORT))
 
 # Listen for incoming connections
 sock.listen(5)
 
 while True:
     # Wait a client connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
+    print('Waiting for a connection')
+    connection, cli_addr = sock.accept()
     try:
-        print('connection from', client_address)
+        print('Connection receive from',cli_addr)
         # Receive the data and retransmit it
         while True:
-            data = connection.recv(64)
-            print('received {!r}'.format(data))
+            data = connection.recv(BUFF_SIZE)
+            print('received {}'.format(data))
             if data:
-                print('sending data to the client..')
+                print('return data to the client..')
                 connection.sendall(data)
             else:
-                print('no data from', client_address)
+                print('no data from client')
                 break
 
     finally:
-        # Clean up the connection
+        #It ensures that the port is closed
         connection.close()
